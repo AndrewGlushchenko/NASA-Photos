066f5ed6d3533fc436176848bcae5d2feab0799a
Andrew Glushchenko <aglushchenko@me.com>
2019-04-05 22:32:01 +0300

    First variant

--- NASA-Photos/Controllers/ViewController.swift
+++ NASA-Photos/Controllers/ViewController.swift
@@ -8,20 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var descriptionView: UITextView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var copyrightLabel: UILabel!
+    
+    @IBOutlet weak var ystdButton: UIButton!
+    
+    @IBOutlet weak var tmrrButton: UIButton!
+    
+    
+    
     var photoInfo: PhotoInfo? {
         didSet {
-            print(Date(), #function, #line, photoInfo ?? "nil")
+            updateUI()
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        descriptionView.delegate = self
         Networking.shared.fetchPhotoInfo { self.photoInfo = $0 }
         
     }
+    
+    func updateUI() {
+        Networking.shared.fetchImage(url: photoInfo?.url) { image in
+            OperationQueue.main.addOperation {
+                self.imageView.image = image
+            }
+        }
+        
+        DispatchQueue.main.async {
+            self.titleLabel.text = self.photoInfo?.title
+            self.descriptionView.text = self.photoInfo?.description
+            //            self.descriptionView.
+            let cprght = self.photoInfo?.copyright
+            
+            if cprght != nil {
+                self.copyrightLabel.textColor = .black
+                self.copyrightLabel.text = cprght
+            }
+            
+        }
+    }
+    
+    
+    
 }
 
 
